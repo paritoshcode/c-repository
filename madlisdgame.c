da3c59da8f7c42625bc1efb4ec4115c6776adb17
Paritosh Dutt <155904430+paritoshcode@users.noreply.github.com>
2025-07-03 11:47:04 +0530

    Update madlisdgame.c

--- madlisdgame.c
+++ madlisdgame.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main(){
     char noun[50] = "";
     char verb[50] = "";
@@ -21,26 +20,18 @@ int main(){
     fgets(adjective2, sizeof(adjective2), stdin);
     adjective2[strlen(adjective2)-1]='\0';
     
-    
     printf("Enter a verb (ending w/ -ing)");
     fgets(verb, sizeof(verb), stdin);
     verb[strlen(verb)-1]='\0';
     
-    
     printf("Enter an adjective (description): ");
     fgets(adjective3, sizeof(adjective3), stdin);
     adjective3[strlen(adjective3)-1]='\0';
-
-
-
+    
     printf("\nToday I went to a %s zoo. \n", adjective1);
     printf("In an exhibit, I saw a %s. \n", noun);
     printf("%s was %s and %s!\n", noun, adjective2, verb);
     printf("I was %s!\n", adjective3);
-
-
-
-
-
+    
     return 0;
 }
